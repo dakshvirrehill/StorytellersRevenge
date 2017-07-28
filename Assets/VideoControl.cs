b901a2e976e6142f951f6199cd8af5f18327a58d
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2017-07-29 03:15:59 +0530

    project files

---
+++ Assets/VideoControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+public class VideoControl : MonoBehaviour {
+	public GameObject MenuGaze;
+	public GameObject Menu;
+	public MediaPlayer mp;
+	public ParticleSystem ps;
+	//public GameObject player;
+	bool enabled=false;
+	void Update () {
+		//DoCheck (mp,Menu,enabled);
+		int times = (int)(mp.Control.GetCurrentTimeMs ()/1000);
+		//Debug.Log (times);
+		if (mp.Control.IsPlaying() && (times == 51||times == 90||times==155||times==210))
+		{
+			pausenMenu (mp, Menu, enabled);
+		}
+		if (mp.Control.IsPlaying () && times > 210 && !ps.isPlaying) {
+			//Debug.Log ("in this function");
+			ps.Play ();
+		}
+		if (mp.Control.IsPlaying () && times >= 230) {
+			ps.Pause ();
+			ps.Clear ();
+		}
+		/*if (mp.Control.IsFinished ()) {
+			ps.Pause ();
+			mp.Control.Rewind ();
+	}*/
+	//private static void DoCheck(MediaPlayer mp, GameObject Menu, bool enabled) {
+		
+		}
+	//}
+	private static void pausenMenu(MediaPlayer mp, GameObject Menu, bool enabled) {
+		mp.Control.Pause ();
+		Menu.SetActive (true);
+		enabled = true;
+	} 
+	public void Continue() {
+		enabled = false;
+		Menu.SetActive (false);
+		mp.Control.Play ();
+		mp.Control.SeekFast (mp.Control.GetCurrentTimeMs () + 1000);
+	}
+	public void EnableDisableMenu() {
+		if (!enabled) {
+			enabled = true;
+			Menu.SetActive (true);
+			mp.Control.Pause ();
+		} else {
+			Menu.SetActive (false);
+			enabled = false;
+			mp.Control.Play ();
+		}
+	}
+	public void rewind() {
+		mp.Control.Rewind ();
+		Menu.SetActive (false);
+		enabled = false;
+		if(mp.Control.IsPaused()) {
+			mp.Control.Play ();
+		}
+	}
+	public void indianTemple() {
+		mp.Control.SeekFast (52000);
+		Menu.SetActive (false);
+		enabled = false;
+		if(mp.Control.IsPaused()) {
+			mp.Control.Play ();
+		}
+	}
+	public void tajMahal() {
+		mp.Control.SeekFast (91000);
+		Menu.SetActive (false);
+		enabled = false;
+		if(mp.Control.IsPaused()) {
+			mp.Control.Play ();
+		}
+	}
+	public void mcLeodGanj() {
+		mp.Control.SeekFast (156000);
+		Menu.SetActive (false);
+		enabled = false;
+		if(mp.Control.IsPaused()) {
+			mp.Control.Play ();
+		}
+	}
+	public void credits() {
+		mp.Control.SeekFast (211000);
+		Menu.SetActive (false);
+		enabled = false;
+		if(mp.Control.IsPaused()) {
+			mp.Control.Play ();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+GoogleVR Version: v1.60.0
+Unity Version: 5.6.2f1
+ProjectTitle: Storyteller's Revenge
+What I found most challenging?
+Almost everything was challenging in this project. From stitching to interaction with unity
+What I liked about this project?
+Learnt a whole lot of stuff as I had to do most of the project
+
+Note: The enable/disable menu button is on the bottom of the screen and after every small clip, the menu automatically appears. You just need to look at the enable/disable menu button to enable/disable menu and click on the menu buttons to get to the next clip or continue or rewind as you please
